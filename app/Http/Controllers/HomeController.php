b8b977a8561c4ce17570b3a3bf142d19731282c2
muhnuramin <muhnuramin956@gmail.com>
2020-09-26 19:48:49 +0700

    tambah cache

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
+use App\article;
 
 class HomeController extends Controller
 {
@@ -11,9 +13,11 @@ class HomeController extends Controller
     public function home()
     {
         // $articles=DB::table('articles')->get();
-        $articles=\App\article::all();
+        // $articles=article::all();
+        // return view('home',['articles'=>$articles]);
+        $articles=Cache::remember('articles',15,function(){
+            return article::all();
+        });
         return view('home',['articles'=>$articles]);
     }
-    
-    
 }
