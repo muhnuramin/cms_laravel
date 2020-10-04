2c106bcc1ad29979320db101c912e61d0219547a
muhnuramin <muhnuramin956@gmail.com>
2020-10-04 20:57:55 +0700

    add-Login

--- cms_kuis/resources/views/login.blade.php
+++ cms_kuis/resources/views/login.blade.php
@@ -3,9 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/login.css">
+    <style>
+
+</style>
 </head>
 <body>
-    <h1>HI</h1>
+    <div class="box">
+        <h2>Sign in</h2>
+        <p>Use your Google Account</p>
+        <form>
+        <div class="inputBox">
+        <input type="email" name="email" required onkeyup="this.setAttribute('value', this.value);"  value="">
+        <label>Username</label>
+        </div>
+        <div class="inputBox">
+        <input type="text" name="text" required onkeyup="this.setAttribute('value', this.value);" value="">
+        <label>Passward</label>
+        </div>
+        <input type="submit" name="sign-in" value="Sign In">
+        </form>
+        </div>
 </body>
 </html>
